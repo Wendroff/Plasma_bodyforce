74b285e7fc3d377003ddead796a4996a299ad82b
Wendroff <624101034@qq.com>
2017-10-26 09:55:14 +0800

    Add the tecplot relavant file

--- plasma_Maden2013.m
+++ plasma_Maden2013.m
@@ -1,4 +1,4 @@
-function [fx,fy,fz] = plasma_Maden2013(x1,y,z1,a0,a1,a2,b0,b1,b2,c,lp,lt,xp0,zp0,theta)
+function [fx,fy,fz] = plasma_Maden2013(x1,y,z1,a0,a1,a2,b0,b1,b2,c,lp,lt,xp0,yp0,zp0,theta)
     %检查输入参数是否正确
     if ( (a0<0) || (b0<0) || (lp<0) || (lt<0) || (c<0))
         error('incorrect input among a0 b0 c lp lt')
@@ -13,8 +13,8 @@ function [fx,fy,fz] = plasma_Maden2013(x1,y,z1,a0,a1,a2,b0,b1,b2,c,lp,lt,xp0,zp0
         X = 0;
     end
     %计算y方向体积力分布
-    if (y>=0)
-        Y = (b1*y + b2*y^2) * exp(-b0*y^0.4);
+    if ((y-yp0)>=0)
+        Y = (b1*(y-yp0) + b2*(y-yp0)^2) * exp(-b0*(y-yp0)^0.4);
     else
         error('incorrect y : y < 0')
     end
